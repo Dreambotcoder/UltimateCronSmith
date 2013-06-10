0ee1a6fcef569085717606e5df0cc9ee57bf415a
ARNO VERHELPEN <arno_gerver@hotmail.com>
2013-06-10 13:26:35 +0200

    Revert "/"
    This reverts commit 224eeeaa72703580c262d1404653d102dfe284c3.

--- org/UltimateCronSmith/GraphicalInterfaces/Heater/HeaterGui.java
+++
@@ -1,9 +0,0 @@
-package org.UltimateCronSmith.GraphicalInterfaces.Heater;
-
-/**
- * @author arno
- * @date 8-6-13
- * @time 21:36
- */
-public class HeaterGui {
-}
--- org/UltimateCronSmith/Smelter/Data/Variables.java
+++
@@ -1,9 +0,0 @@
-package org.UltimateCronSmith.Smelter.Data;
-
-/**
- * @author arno
- * @date 9-6-13
- * @time 0:29
- */
-public class Variables {
-}
